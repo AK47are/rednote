3c11aa2d7f9c8663df918143d4a3d5d1007ecda0
Ning001 <2825601439@qq.com>
2026-03-21 11:53:14 +0800

    feat(frontend): 添加'我的关注'界面所需的后端 Api

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -2,3 +2,4 @@ import request from '@/utils/request'
 
 export const getPostListApi = (params) => request.get('/posts/list', {params})
 export const getPostDetailApi = (postId) => request.get(`/postsDetails/${postId}`)
+export const getBloggerPostsApi = (bloggerId) => request.get(`/posts/list/${bloggerId}`)
