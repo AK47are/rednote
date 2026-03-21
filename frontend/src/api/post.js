87ca3a9d53cbf2438079ebe6e1500ea9c67b7600
Ning001 <2825601439@qq.com>
2026-03-21 11:46:34 +0800

    feat(frontend): 添加帖子详情卡片并配置所需 Api

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -1,3 +1,4 @@
 import request from '@/utils/request'
 
 export const getPostListApi = (params) => request.get('/posts/list', {params})
+export const getPostDetailApi = (postId) => request.get(`/postsDetails/${postId}`)
