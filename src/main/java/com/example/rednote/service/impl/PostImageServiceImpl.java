3cf7c53008787d82d88615a9ee7b922c30b2511e
Ning001 <2825601439@qq.com>
2026-03-21 22:43:44 +0800

    feat(backend): 实现根据帖子 id 获取帖子图片列表的接口

--- src/main/java/com/example/rednote/service/impl/PostImageServiceImpl.java
+++ src/main/java/com/example/rednote/service/impl/PostImageServiceImpl.java
@@ -1,12 +1,29 @@
 package com.example.rednote.service.impl;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.rednote.common.utils.MinioUtils;
+import com.example.rednote.mapper.PostImageMapper;
+import com.example.rednote.model.po.PostImagePO;
 import com.example.rednote.service.PostImageService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 @AllArgsConstructor
 public class PostImageServiceImpl implements PostImageService {
 
+    private final PostImageMapper postImageMapper;
+    private final MinioUtils minioUtils;
 
+    @Override
+    public List<String> list(Integer postId) {
+        List<PostImagePO> postImagePOS = postImageMapper
+                .selectList(new LambdaQueryWrapper<PostImagePO>().eq(PostImagePO::getPostId, postId));
+        return postImagePOS.stream().map(postImagePO -> {
+            postImagePO.setUrl(minioUtils.getPublicUrl(postImagePO.getUrl()));
+            return postImagePO.getUrl();
+        }).toList();
+    }
 }
