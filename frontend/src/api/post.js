d187de3a12ef3c1fe58abf2726ebe96fd5b68231
Ning001 <2825601439@qq.com>
2026-03-22 11:23:27 +0800

    feat(frontend): 封装根据用户 id 获取帖子列表 Api

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -1,3 +1,4 @@
 import request from "@/utils/request"
 export const getPostListApi = (topicId) => request.get(`/posts/list/${topicId}`)
 export const getPostDetailApi = (postId) => request.get(`/posts/details/${postId}`)
+export const getPostByUserIdApi = (userId) => request.get(`/posts/list/user/${userId}`)
