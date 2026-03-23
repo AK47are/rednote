4803b497fbb63765aa086d22a357fe8b25486293
AK47are <3318517070@qq.com>
2026-03-23 18:15:58 +0800

    style(backend): 清理无关 import

--- src/main/java/com/example/rednote/service/PostTopicService.java
+++ src/main/java/com/example/rednote/service/PostTopicService.java
@@ -1,9 +1,4 @@
 package com.example.rednote.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.rednote.model.dto.PostDTO;
-import com.example.rednote.model.po.PostPO;
-
-
 public interface PostTopicService {
 }
