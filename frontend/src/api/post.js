d4a6728b50907f2c288c9e253da01467e461e5c1
hulunkunzao <2380381330@qq.com>
2026-03-23 18:55:38 +0800

    feat(frontend): 封装添加帖子 Api

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -2,3 +2,4 @@ import request from "@/utils/request"
 export const getPostListApi = (topicId) => request.get(`/posts/list/${topicId}`)
 export const getPostDetailApi = (postId) => request.get(`/posts/details/${postId}`)
 export const getPostByUserIdApi = (userId) => request.get(`/posts/list/user/${userId}`)
+export const createPostApi = (postDTO) => request.post('/posts/insert', postDTO)
